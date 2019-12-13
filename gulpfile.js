6021f7c9b7e93635bb9d20f986281499727b889f
lavp <lavp@nextlav.xyz>
2019-12-13 14:38:15 +0900

    開発環境をgulpで再構築

--- gulpfile.js
+++ gulpfile.js
@@ -12,31 +12,53 @@ const mergeStream = require('merge-stream');
 const jshint = require('gulp-jshint');
 const uglify = require('gulp-uglify');
 const browserSync = require('browser-sync').create();
-const FtpDeploy = require('ftp-deploy');
 const rename = require('gulp-rename');
-const ftpDeploy = new FtpDeploy();
+var sftp = require('gulp-sftp-up4');
 
 
-// ルート直下のphp
-function php() {
-  return gulp
-      .src('./src/*.php')
-      .pipe(gulp.dest('./build/themes/atea_company/'));
-}
-// ルート直下のstyle.css
-function stylecss() {
-  return gulp
-      .src(['./src/style.css'], {sourcemaps: true})
-      .pipe(autoprefixer({grid:'autoplace'}))
-      .pipe(gulp.dest('./build/themes/atea_company/'));
-}
 
 //WordPressテーマの変換
-function global_resources() {
+function convert_wp_theme() {
   const in_dir = './wp-theme/';
-  const out_dir = './build/themes/atea_company/';
+  const out_dir = './Dist/wp-theme/';
   return mergeStream(
-      // css
+      // in style css
+      gulp.src(in_dir+'/style/**/*.scss', {sourcemaps: true})
+          .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
+          .pipe(sass())
+          .pipe(autoprefixer({grid:'autoplace'}))
+          .pipe(cssmin())
+          .pipe(gulp.dest(out_dir+'style')),
+      // in root css
+      gulp.src(in_dir+'style.css', {sourcemaps: true})
+          .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
+          .pipe(sass())
+          .pipe(autoprefixer({grid:'autoplace'}))
+          .pipe(gulp.dest(out_dir)),
+      //js
+      gulp.src(in_dir+'/js/**/*.js')
+          .pipe(jshint())
+          //.pipe(uglify())
+          .pipe(gulp.dest(out_dir+'js')),
+      // images
+      gulp.src(in_dir+'/images/**/*')
+          .pipe(imagemin())
+          .pipe(gulp.dest(out_dir+'images')),
+      //root php
+      gulp.src(in_dir+'*.php')
+        .pipe(gulp.dest(out_dir)),
+      //mpa parts
+      gulp.src(in_dir+'/mpa/*.php')
+        .pipe(gulp.dest(out_dir+'/mpa'))
+  );
+}
+
+//ar_scan_appの変換
+function convert_ar_scan_app() {
+  const in_dir = './ar_scan_app/';
+  const out_dir = './Dist/ar/';
+  return mergeStream(
+      // style
       gulp.src(in_dir+'/style/**/*.scss', {sourcemaps: true})
           .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
           .pipe(sass())
@@ -46,116 +68,62 @@ function global_resources() {
       //js
       gulp.src(in_dir+'/js/**/*.js')
           .pipe(jshint())
-          .pipe(uglify())
-          .pipe(gulp.dest(out_dir+'script')),
-      // images
-      gulp.src(in_dir+'/images/**/*')
-          .pipe(imagemin())
-          .pipe(gulp.dest(out_dir+'images'))
-  );
-}
-
-//PHP_BLOCKS
-function php_blocks() {
-  const in_dir = './src/php_blocks/';
-  const out_dir = './build/themes/atea_company/php_blocks/';
-  return mergeStream(
-      //php
+          //.pipe(uglify())
+          .pipe(gulp.dest(out_dir+'js')),
+      // assets
+      gulp.src(in_dir+'/assets/**/*')
+          .pipe(gulp.dest(out_dir+'assets')),
+      //root php
       gulp.src(in_dir+'*.php')
-          .pipe(gulp.dest(out_dir)),
-      // css
-      gulp.src(in_dir+'style/**/*.scss', {sourcemaps: true})
-          .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
-          .pipe(sass())
-          .pipe(autoprefixer({grid:'autoplace'}))
-          .pipe(cssmin())
-          .pipe(gulp.dest(out_dir+'style/'))
+        .pipe(gulp.dest(out_dir)),
+      //ui parts
+      gulp.src(in_dir+'/ui_parts/*.php')
+        .pipe(gulp.dest(out_dir+'/ui_parts'))
   );
 }
 
-//各投稿タイプの関連ファイルのまとまり
-function post_type_folder() {
-  const in_dir = './src/*_post_type/';
-  const out_dir = './build/themes/atea_company/';
-  return mergeStream(
-      // css
-      gulp.src(in_dir+'*.scss', {sourcemaps: true})
-          .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
-          .pipe(sass())
-          .pipe(autoprefixer({grid:'autoplace'}))
-          .pipe(cssmin())
-          .pipe(rename(function(path){
-            path.dirname -= 'post_type';
-          }))
-          .pipe(gulp.dest(out_dir+'style/')),
-      //js
-      gulp.src(in_dir+'*.js')
-          .pipe(jshint())
-          .pipe(uglify())
-          .pipe(rename(function(path){
-            path.dirname -= 'post_type';
-          }))
-          .pipe(gulp.dest(out_dir+'script/')),
-      // images
-      gulp.src(in_dir+'images/**/*')
-          .pipe(imagemin())
-          .pipe(rename(function(path){
-            path.dirname -= 'post_type';
-          }))
-          .pipe(gulp.dest(out_dir+'images/')),
-      //php
-      gulp.src(in_dir+'*.php')
-      .pipe(rename(function(path){
-        path.dirname -= 'post_type';
-      }))
-          .pipe(gulp.dest(out_dir))
-  );
-}
-
-// browserSync 未使用
-const browserSyncOption = {
-  port: 8080,
-  server: {
-    baseDir: './build/themes/atea_company/'
-    // index: 'index.html',
-  },
-  reloadOnRestart: true,
-};
-function browsersync(done) {
-  browserSync.init(browserSyncOption);
-  done();
-}
-
-// ftp
-function ftp(done) {
-  var config = {
-    user: 'ae153ulm6t_trunk',
-    password: "r3GJ2g_25J",
-    host: "150.60.157.168",
-    port: 21,
-    localRoot: "./build/themes/atea_company/",
-    remoteRoot: '/html/atea/wp-content/themes/atea_dev_a',
-    include: ['*', '**/*'],
-    deleteRemote: true
+//upload_wp_theme
+function upload_wp_theme(){
+  const options = {
+    host: 'www6.conoha.ne.jp',
+    port: 8022,
+    user: 'c0704253',
+    remotePlatform: 'unix',
+    remotePath: '/home/c0704253/public_html/step.nextlav.xyz/wp-content/themes/match_plate',
+    key:'/Users/lavp/.ssh/key-2019-12-13-14-19.pem'
   };
-  ftpDeploy.deploy(config, function(err) {
-    if(err) console.log(err);
-    else console.log('finished');
-  });
-  done();
+  return mergeStream(
+    gulp.src('./Dist/wp-theme/**/*')
+      .pipe(sftp(options))
+    );
+};
+
+//upload_ar_scan_app
+function upload_ar_scan_app(){
+  const options = {
+    host: 'www6.conoha.ne.jp',
+    port: 8022,
+    user: 'c0704253',
+    remotePlatform: 'unix',
+    remotePath: '/home/c0704253/public_html/step.nextlav.xyz/ar',
+    key:'/Users/lavp/.ssh/key-2019-12-13-14-19.pem'
+  };
+  return mergeStream(
+    gulp.src('./Dist/ar/**/*')
+    .pipe(sftp(options))
+  );
 }
 
 
 function watch() {
-  //新しいの
-  gulp.watch('./src/*_post_type/**/*',post_type_folder);
-  gulp.watch('./src/php_blocks/**/*', php_blocks);
-  gulp.watch('./src/global_resources/**/*', global_resources);
-  gulp.watch('./src/**/*.php', php)
-  gulp.watch('./src/style.css', stylecss);
-
-  gulp.watch('./build/themes/atea_company/**/*',ftp);
+  //変換処理
+  gulp.watch('./wp-theme/**/*',convert_wp_theme);
+  gulp.watch('./ar_scan_app//**/*',convert_ar_scan_app);
+  //アップロード
+  gulp.watch('./Dist/wp-theme/**/*',upload_wp_theme);
+  gulp.watch('./Dist/ar/**/*',upload_ar_scan_app);
 }
 
-exports.default = gulp.series(php,stylecss,global_resources,php_blocks,post_type_folder,ftp,watch);
+exports.default = gulp.series(convert_wp_theme,convert_ar_scan_app,upload_wp_theme,upload_ar_scan_app,watch);
+//exports.default = gulp.series(upload_wp_theme,upload_ar_scan_app);
 
