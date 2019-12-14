c21aeb618831b104990a81a225cfa893a492feb8
lavp <lavp@nextlav.xyz>
2019-12-14 12:56:58 +0900

    gulpの設定をアップデート - 変更されたファイルだけが処理されるように - 変更されたものだけアップロードされるように - ソースマップが出力されるように - JSをES5仕様にトランスパイルするように

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const sass = require('gulp-sass');
 const sassGlob = require('gulp-sass-glob');
 const cssmin = require('gulp-cssmin');
 const plumber = require('gulp-plumber');
-const notify = require("gulp-notify");
+const notify = require('gulp-notify');
 const concat = require('gulp-concat');
 const autoprefixer = require('gulp-autoprefixer');
 // const gih = require("gulp-include-html");
@@ -11,119 +11,164 @@ const imagemin = require('gulp-imagemin');
 const mergeStream = require('merge-stream');
 const jshint = require('gulp-jshint');
 const uglify = require('gulp-uglify');
+const babel = require('gulp-babel');
 const browserSync = require('browser-sync').create();
 const rename = require('gulp-rename');
-var sftp = require('gulp-sftp-up4');
-
-
+const sftp = require('gulp-sftp-up4');
+const sourcemaps = require('gulp-sourcemaps');
+const changed = require('gulp-changed');
 
 //WordPressテーマの変換
 function convert_wp_theme() {
-  const in_dir = './wp-theme/';
+	const in_dir = './wp-theme/';
   const out_dir = './Dist/wp-theme/';
-  return mergeStream(
-      // in style css
-      gulp.src(in_dir+'/style/**/*.scss', {sourcemaps: true})
-          .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
-          .pipe(sass())
-          .pipe(autoprefixer({grid:'autoplace'}))
-          .pipe(cssmin())
-          .pipe(gulp.dest(out_dir+'style')),
-      // in root css
-      gulp.src(in_dir+'style.css', {sourcemaps: true})
-          .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
-          .pipe(sass())
-          .pipe(autoprefixer({grid:'autoplace'}))
-          .pipe(gulp.dest(out_dir)),
-      //js
-      gulp.src(in_dir+'/js/**/*.js')
-          .pipe(jshint())
-          //.pipe(uglify())
-          .pipe(gulp.dest(out_dir+'js')),
-      // images
-      gulp.src(in_dir+'/images/**/*')
-          .pipe(imagemin())
-          .pipe(gulp.dest(out_dir+'images')),
-      //root php
-      gulp.src(in_dir+'*.php')
-        .pipe(gulp.dest(out_dir)),
-      //mpa parts
-      gulp.src(in_dir+'/mpa/*.php')
-        .pipe(gulp.dest(out_dir+'/mpa'))
-  );
+  const options = {
+		host: 'www6.conoha.ne.jp',
+		port: 8022,
+		user: 'c0704253',
+		remotePlatform: 'unix',
+		remotePath:
+			'/home/c0704253/public_html/step.nextlav.xyz/wp-content/themes/match_plate',
+		key: '/Users/lavp/.ssh/key-2019-12-13-14-19.pem'
+	};
+	return mergeStream(
+		// in style css
+		gulp
+      .src(in_dir + '/style/**/*.scss')
+      .pipe(changed(out_dir + 'style'))
+			.pipe(sourcemaps.init())
+			.pipe(
+				plumber({ errorHandler: notify.onError('Error: <%= error.message %>') })
+			)
+			.pipe(sass())
+			.pipe(autoprefixer({ grid: 'autoplace' }))
+      .pipe(cssmin())
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest(out_dir + 'style'))
+      .pipe(sftp(options)),
+		// in root css
+		gulp
+      .src(in_dir + 'style.css')
+      .pipe(changed(out_dir))
+      .pipe(sourcemaps.init())
+			.pipe(
+				plumber({ errorHandler: notify.onError('Error: <%= error.message %>') })
+			)
+			.pipe(sass())
+      .pipe(autoprefixer({ grid: 'autoplace' }))
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest(out_dir))
+      .pipe(sftp(options)),
+		//js
+		gulp
+      .src(in_dir + '/js/**/*.js')
+      .pipe(changed(out_dir + 'js'))
+      .pipe(sourcemaps.init())
+			.pipe(jshint())
+      .pipe(babel({
+        "presets": ["@babel/preset-env"]
+      }))
+      .pipe(uglify())
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest(out_dir + 'js'))
+      .pipe(sftp(options)),
+		// images
+		gulp
+      .src(in_dir + '/images/**/*')
+      .pipe(changed(out_dir + 'images'))
+			.pipe(imagemin())
+      .pipe(gulp.dest(out_dir + 'images'))
+      .pipe(sftp(options)),
+		//root php
+    gulp.src(in_dir + '*.php')
+    .pipe(changed(out_dir))
+    .pipe(sourcemaps.init())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(out_dir))
+    .pipe(sftp(options)),
+		//mpa parts
+    gulp.src(in_dir + '/mpa/*.php')
+    .pipe(changed(out_dir + '/mpa'))
+    .pipe(sourcemaps.init())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(out_dir + '/mpa'))
+    .pipe(sftp(options))
+	);
 }
 
 //ar_scan_appの変換
 function convert_ar_scan_app() {
-  const in_dir = './ar_scan_app/';
+	const in_dir = './ar_scan_app/';
   const out_dir = './Dist/ar/';
-  return mergeStream(
-      // style
-      gulp.src(in_dir+'/style/**/*.scss', {sourcemaps: true})
-          .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
-          .pipe(sass())
-          .pipe(autoprefixer({grid:'autoplace'}))
-          .pipe(cssmin())
-          .pipe(gulp.dest(out_dir+'style')),
-      //js
-      gulp.src(in_dir+'/js/**/*.js')
-          .pipe(jshint())
-          //.pipe(uglify())
-          .pipe(gulp.dest(out_dir+'js')),
-      // assets
-      gulp.src(in_dir+'/assets/**/*')
-          .pipe(gulp.dest(out_dir+'assets')),
-      //root php
-      gulp.src(in_dir+'*.php')
-        .pipe(gulp.dest(out_dir)),
-      //ui parts
-      gulp.src(in_dir+'/ui_parts/*.php')
-        .pipe(gulp.dest(out_dir+'/ui_parts'))
-  );
-}
-
-//upload_wp_theme
-function upload_wp_theme(){
   const options = {
-    host: 'www6.conoha.ne.jp',
-    port: 8022,
-    user: 'c0704253',
-    remotePlatform: 'unix',
-    remotePath: '/home/c0704253/public_html/step.nextlav.xyz/wp-content/themes/match_plate',
-    key:'/Users/lavp/.ssh/key-2019-12-13-14-19.pem'
-  };
-  return mergeStream(
-    gulp.src('./Dist/wp-theme/**/*')
-      .pipe(sftp(options))
-    );
-};
-
-//upload_ar_scan_app
-function upload_ar_scan_app(){
-  const options = {
-    host: 'www6.conoha.ne.jp',
-    port: 8022,
-    user: 'c0704253',
-    remotePlatform: 'unix',
-    remotePath: '/home/c0704253/public_html/step.nextlav.xyz/ar',
-    key:'/Users/lavp/.ssh/key-2019-12-13-14-19.pem'
-  };
-  return mergeStream(
-    gulp.src('./Dist/ar/**/*')
+		host: 'www6.conoha.ne.jp',
+		port: 8022,
+		user: 'c0704253',
+		remotePlatform: 'unix',
+		remotePath: '/home/c0704253/public_html/step.nextlav.xyz/ar',
+		key: '/Users/lavp/.ssh/key-2019-12-13-14-19.pem'
+	};
+	return mergeStream(
+		// style
+		gulp
+      .src(in_dir + '/style/**/*.scss')
+      .pipe(changed(out_dir + 'style'))
+      .pipe(sourcemaps.init())
+			.pipe(
+				plumber({ errorHandler: notify.onError('Error: <%= error.message %>') })
+			)
+			.pipe(sass())
+			.pipe(autoprefixer({ grid: 'autoplace' }))
+      .pipe(cssmin())
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest(out_dir + 'style'))
+      .pipe(sftp(options)),
+		//js
+		gulp
+      .src(in_dir + '/js/**/*.js')
+      .pipe(changed(out_dir + 'js'))
+      .pipe(sourcemaps.init())
+			.pipe(jshint())
+      .pipe(babel({
+        "presets": ["@babel/preset-env"]
+      }))
+      .pipe(uglify())
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest(out_dir + 'js'))
+      .pipe(sftp(options)),
+		// assets
+    gulp.src(in_dir + '/assets/**/*')
+    .pipe(changed(out_dir + 'assets'))
+    .pipe(gulp.dest(out_dir + 'assets'))
+    .pipe(sftp(options)),
+		//root php
+    gulp.src(in_dir + '*.php')
+    .pipe(changed(out_dir))
+    .pipe(sourcemaps.init())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(out_dir))
+    .pipe(sftp(options)),
+		//ui parts
+    gulp.src(in_dir + '/ui_parts/*.php')
+    .pipe(changed(out_dir + '/ui_parts'))
+    .pipe(sourcemaps.init())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(out_dir + '/ui_parts'))
     .pipe(sftp(options))
-  );
+	);
 }
+
 
 
 function watch() {
-  //変換処理
-  gulp.watch('./wp-theme/**/*',convert_wp_theme);
-  gulp.watch('./ar_scan_app/**/*',convert_ar_scan_app);
-  //アップロード
-  gulp.watch('./Dist/wp-theme/**/*',upload_wp_theme);
-  gulp.watch('./Dist/ar/**/*',upload_ar_scan_app);
+	//変換処理
+	gulp.watch('./wp-theme/**/*', convert_wp_theme);
+	gulp.watch('./ar_scan_app/**/*', convert_ar_scan_app);
 }
 
-exports.default = gulp.series(convert_wp_theme,convert_ar_scan_app,upload_wp_theme,upload_ar_scan_app,watch);
+exports.default = gulp.series(
+	convert_wp_theme,
+	convert_ar_scan_app,
+	watch
+);
 //exports.default = gulp.series(upload_wp_theme,upload_ar_scan_app);
-
